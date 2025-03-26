1635e4771de716ebd65d559249d4cc9ffe9a229b
Chandima Ranaweera <chan4lk@gmail.com>
2025-03-26 12:16:32 +0530

    mcp adapter is working

---
+++ langgraph-app/workflow-engine/src/mcp_agent/graph.py
@@ -0,0 +1,22 @@
+# graph.py
+from contextlib import asynccontextmanager
+from langchain_mcp_adapters.client import MultiServerMCPClient
+from langgraph.prebuilt import create_react_agent
+from langchain_anthropic import ChatAnthropic
+from langchain_openai import ChatOpenAI
+
+model = ChatOpenAI(model="gpt-4o-mini") 
+
+@asynccontextmanager
+async def make_graph():
+    async with MultiServerMCPClient(
+        {
+            "weather": {
+                # make sure you start your weather server on port 8000
+                "url": "http://localhost:8000/sse",
+                "transport": "sse",
+            }
+        }
+    ) as client:
+        agent = create_react_agent(model, client.get_tools())
+        yield agent
