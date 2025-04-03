19a40089db0af4a651aa18c8e0fcd766078d39a2
Chandima Ranaweera <chan4lk@gmail.com>
2025-04-03 17:40:30 +0530

    Add Postgres service and update graph workflow with state management

--- langgraph-app/workflow-engine/src/mcp_agent/graph.py
+++ langgraph-app/workflow-engine/src/mcp_agent/graph.py
@@ -4,8 +4,20 @@ from langchain_mcp_adapters.client import MultiServerMCPClient
 from langgraph.prebuilt import create_react_agent
 from langchain_anthropic import ChatAnthropic
 from langchain_openai import ChatOpenAI
+from dataclasses import dataclass, field
+from typing import Sequence
+
+from langchain_core.messages import AnyMessage
+from langgraph.graph import add_messages, StateGraph, START, END
+from typing_extensions import Annotated
 
 model = ChatOpenAI(model="gpt-4o-mini") 
+
+@dataclass
+class InputState:
+    messages: Annotated[Sequence[AnyMessage], add_messages] = field(
+        default_factory=list
+    )
 
 @asynccontextmanager
 async def make_graph():
@@ -18,7 +30,7 @@ async def make_graph():
             "-i", 
             "--rm", 
             "mcp/postgres", 
-            "postgresql://postgres:mysecretpassword@host.docker.internal:5432/kaya-ai-platform"],
+            "postgresql://elb:postgres@host.docker.internal:5432/elb"],
             "transport":"stdio"
         },
             "weather": {
@@ -29,4 +41,11 @@ async def make_graph():
         }
     ) as client:
         agent = create_react_agent(model, client.get_tools())
-        yield agent
+
+        sub_graph_workflow = StateGraph(InputState)
+        sub_graph_workflow.add_node("executor", agent)
+        sub_graph_workflow.add_edge(START, "executor")
+        sub_graph_workflow.add_edge("executor", END)
+        sub_graph = sub_graph_workflow.compile()
+
+        yield sub_graph
