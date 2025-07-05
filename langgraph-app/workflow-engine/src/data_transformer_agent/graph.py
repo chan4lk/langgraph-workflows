aeba34f2758965e19b085acc253468f479f06d07
Chandima Ranaweera <chan4lk@gmail.com>
2025-07-05 20:10:44 +0530

    feat: implement data transformation workflow with loader and cleaner agents

---
+++ langgraph-app/workflow-engine/src/data_transformer_agent/graph.py
@@ -0,0 +1,68 @@
+from langchain_openai import ChatOpenAI
+from dataclasses import dataclass, field
+from langgraph.graph import StateGraph, START, END, add_messages
+from langchain_core.messages import AnyMessage, AIMessage
+from typing import Sequence
+from typing_extensions import Annotated
+from ai_data_science_team.agents import DataLoaderToolsAgent, DataCleaningAgent
+import os
+import pandas as pd
+from datetime import datetime
+
+@dataclass
+class State:
+    messages: Annotated[Sequence[AnyMessage], add_messages] = field(
+        default_factory=list
+    )
+    artifact: dict = field(
+        default_factory=dict
+    )
+    cleaned_data: dict = field(
+        default_factory=dict
+    )
+
+llm = ChatOpenAI(model="gpt-4o-mini")
+LOG      = True
+LOG_PATH = os.path.join(os.getcwd(), "logs/")
+
+# Make a data loader agent
+data_loader_agent = DataLoaderToolsAgent(
+    llm, 
+    invoke_react_agent_kwargs={"recursion_limit": 10}, 
+)
+
+data_cleaning_agent = DataCleaningAgent(
+    llm,
+    log=LOG,
+    log_path=LOG_PATH,
+)
+
+def data_loader_node(state: State):
+   data_loader_agent.invoke_agent(state.messages[-1].content)
+   response = data_loader_agent.get_ai_message()
+   artifact = data_loader_agent.get_artifacts()
+   return { "messages": state.messages + [AIMessage(content=response)], "artifact": artifact }
+
+def data_cleaning_node(state: State):
+   dict_data = state.artifact
+   df = pd.DataFrame(dict_data)
+   today = datetime.now().strftime("%Y-%m-%d")
+   data_cleaning_agent.invoke_agent(
+    data_raw=df,
+    user_instructions=f"Today's date is {today}. Remove rows where the date is is not 11/12/2024s. Don't remove outliers when cleaning the data.",
+    max_retries=3,
+    retry_count=0
+   ) 
+   response = data_cleaning_agent.get_response()
+   cleaned_data = data_cleaning_agent.get_data_cleaned()
+   return { "messages": state.messages + [AIMessage(content="Data cleaned successfully")], "cleaned_data": cleaned_data.to_dict() }
+
+workflow = StateGraph(State)
+workflow.add_node("data_loader", data_loader_node)
+workflow.add_node("data_cleaning", data_cleaning_node)
+
+workflow.add_edge(START, "data_loader")
+workflow.add_edge("data_loader", "data_cleaning")
+workflow.add_edge("data_cleaning", END)
+
+graph = workflow.compile()
